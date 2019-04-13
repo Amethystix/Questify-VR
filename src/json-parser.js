8422d70e57d30f11307b30c10975249234bdb875
Lauren <lad475@nyu.edu>
2019-04-13 12:19:27 -0400

    Working on json parser

--- src/json-parser.js
+++ src/json-parser.js
@@ -1,3 +1,24 @@
 const data = require('./scene.json');
 
 // TODO: Figure out the best way to parse the json into a scene
+function parseJSON(json, sceneName) {
+  let sceneIsLoaded = false;
+  data.scenes.forEach(scene => {
+    if (!sceneName) {
+      if (scene.isDefault) {
+        loadScene(scene);
+        sceneIsLoaded = true;
+      } 
+    } else if (sceneName === scene.name) {
+      loadScene(scene);
+      sceneIsLoaded = true;
+    }
+  });
+  if (!sceneIsLoaded) {
+    console.error(sceneName ? `The scene ${sceneName} does not exist` : 'There is no default scene provided');
+  }
+}
+
+function loadScene(scene) {
+  
+}
