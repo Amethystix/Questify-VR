ba6a2db34810b2e3cf420fa6122bec79e43909f6
Lauren <lad475@nyu.edu>
2019-04-13 12:04:56 -0400

    Changing aframe to peer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import aframe from 'aframe';
+
 // import parseJson from './parse-json';
 
 function main() {
-  console.log(aframe);
   // const scene = parseJson('scene.json');
   // makeScene(scene);
 }
---
+++ src/make-scene.js
@@ -0,0 +1,5 @@
+function makeScene() {
+  
+}
+
+export default makeScene;
